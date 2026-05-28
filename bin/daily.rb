43769201a567180791733df6c9b1cac8f67a73fa
Calum Murphy <calummurphy@Calums-MacBook-Air.local>
2026-05-28 15:30:58 +0100

    Handling Notes input and styling

--- bin/daily.rb
+++ bin/daily.rb
@@ -7,6 +7,9 @@ require "time"
 
 require_relative '../lib/start'
 
+require "pastel"
+require "terminal-table"
+
 module Daily
   module CLI
     module Commands
@@ -43,11 +46,11 @@ module Daily
 
         def call(date: nil, **)
 
-          
-          puts 'Results:'
+        
 
           file_path = File.expand_path('../data/templates/results_test.json', __dir__)
           data = JSON.parse(File.read(file_path))
+          pastel = Pastel.new
 
           date ||= Time.now.utc.to_date.to_s
 
@@ -70,21 +73,41 @@ module Daily
 
             entry = true
 
+            rows = []
+            rows << ["Tester", run['tester']]
+            rows << ["Date", Time.parse(run["start-time"]).utc.to_date]
+            rows << ["Start", start_time.utc.strftime("%H:%M:%S")]
+            rows << ["End", end_time.utc.strftime("%H:%M:%S")]
+            rows << ["Duration", format('%02d:%02d:%02d', hours, minutes, seconds)]
+
+            details_table = Terminal::Table.new :title => "Test details", :rows => rows
+
+            puts details_table
+
+
+
             
-            puts "======================================"
-            puts "Tester: #{run['tester']}"
-            puts "Date: #{Time.parse(run["start-time"]).utc.to_date}"
-            puts "Start:  #{start_time.utc.strftime("%H:%M:%S")}"
-            puts "End:    #{end_time.utc.strftime("%H:%M:%S")}"
-            puts "Duration: #{format('%02d:%02d:%02d', hours, minutes, seconds)}"
+            # puts "======================================"
+            # puts "Tester: #{run['tester']}"
+            # puts "Date: #{Time.parse(run["start-time"]).utc.to_date}"
+            # puts "Start:  #{start_time.utc.strftime("%H:%M:%S")}"
+            # puts "End:    #{end_time.utc.strftime("%H:%M:%S")}"
+            # puts "Duration: #{format('%02d:%02d:%02d', hours, minutes, seconds)}"
+            # 
+            tasks = []
 
             run['results'].each do |result|
               status = result['passed'] ? "PASS" : "FAIL"
-              puts "#{status} - #{result['title']}"
+              if status == "PASS"
+                tasks << [pastel.green(status), pastel.green(result['title']), result['notes']]
+              else
+                tasks << [pastel.bold.red(status), pastel.bold.red(result['title']), result['notes']]
+              end
             end
-            puts "\n"
+            results_table = Terminal::Table.new :title => "Results for #{date}", :headings => ['Outcome', 'Task', 'Notes'], :rows => tasks
+            puts results_table
           end
-          puts "System check pending." unless entry
+          puts "Today's system check pending..." unless entry
         end
       end
 
