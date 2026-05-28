f048de23fe28c0b9006308c9819c9221ddce82ec
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2026-05-28 13:21:03 +0100

    Adds structure

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ source 'https://rubygems.org'
 # gem "rails"
 
 gem 'dry-cli', '~> 1.4'
+
+gem 'tty-prompt', '~> 0.23.1'
--- lib/start.rb
+++ lib/start.rb
@@ -1,15 +1,30 @@
 # frozen_string_literal: true
 
+require 'json'
+
 module Daily
   class Start
     def initialize
+      steps_file = File.open "#{__dir__}/../data/shared/steps.json"
+      self.class.instance_variable_set(:@step_data, JSON.parse(steps_file.read))
+      results_template_file = File.open "#{__dir__}/../data/templates/results.json"
+      self.class.instance_variable_set(:@results, JSON.parse(results_template_file.read))
       run
     end
 
     private
 
     def run
-      puts 'Hello World1'
+      steps_data = self.class.instance_variable_get(:@step_data)
+      results = self.class.instance_variable_get(:@results)
+
+      results['start-time'] = Time.new.utc
+      sleep(50)
+      results['end-time'] = Time.new.utc
+
+      File.write()
+
+      puts steps_data
     end
   end
 end
