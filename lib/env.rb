2555b40b57f253f7955bf9165eedabd4d99695a8
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2026-05-29 16:19:51 +0100

    Update env.rb

--- lib/env.rb
+++ lib/env.rb
@@ -14,22 +14,27 @@ module Daily
       prompt = TTY::Prompt.new
       env_data = File.exist?(env_file_location) ? Dotenv.load(env_file_location) : ''
       puts
-      testers = prompt.ask('What are you\'re testers name (names must be comma separated e.g. name1,name2) ',
+      testers = prompt.ask('What are you\'re testers name (names must be comma separated e.g. name1,name2)? ',
                            value: env_data['TESTERS'].nil? ? '' : env_data['TESTERS']) do |q|
         q.required true
         q.modify :strip
       end
       puts
       hpcs = prompt.ask(
-        'What\'s the names of the HPC\'s (names must be comma seperated e.g. hpc1,hpc2) ', value: env_data['HPCS'].nil? ? '' : env_data['HPCS']
+        'What\'s the names of the HPC\'s (names must be comma seperated e.g. hpc1,hpc2)? ', value: env_data['HPCS'].nil? ? '' : env_data['HPCS']
       ) do |q|
+        q.required true
+        q.modify :strip
+      end
+      puts
+      webhook_key = prompt.ask('What\'s the webhook api key you would like to use?') do |q|
         q.required true
         q.modify :strip
       end
       puts
       puts 'Your .env has been saved'
       puts
-      File.write(env_file_location, "TESTERS=#{testers}\nHPCS=#{hpcs}")
+      File.write(env_file_location, "TESTERS=#{testers}\nHPCS=#{hpcs}\nWEBHOOK_API_KEY=#{webhook_key}")
     end
   end
 end
